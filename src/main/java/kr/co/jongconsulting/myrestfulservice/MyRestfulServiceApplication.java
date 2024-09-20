f2abc4ebdab6185105a6016611dcd26d26708efd
leejonghyun93 <fhohffodf93@gmail.com>
2024-09-21 01:14:53 +0900

    1. 초기셋팅(1)

---
+++ src/main/java/kr/co/jongconsulting/myrestfulservice/MyRestfulServiceApplication.java
@@ -0,0 +1,19 @@
+package kr.co.jongconsulting.myrestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MyRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = SpringApplication.run(MyRestfulServiceApplication.class, args);
+
+		String[] allBeanNams = ac.getBeanDefinitionNames();
+		for (String beanName : allBeanNams){
+			System.out.println(beanName);
+		}
+	}
+
+}
